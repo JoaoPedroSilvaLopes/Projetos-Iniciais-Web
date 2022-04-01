7cc45596dae5fe39d9fdbde2fbecdbb6502f3987
JoaoPedroSilvaLopes <joaopedroS.L@hotmail.com>
2022-03-31 23:03:12 -0300

    Implementações de funções para gerar caracteristicas especificas dos pokemons

--- dadosPokemon.js
+++ dadosPokemon.js
@@ -4,26 +4,34 @@ dos pokémons como: numero da pokedex, tipo, sprites e
 descrição da pokedex.
 */
 
+/*
+HP:
+- Deve ser verde quando: HP-ATUAL > HP/2
+- Deve ser amarelo quando: HP/5 <= HP-ATUAL <= HP/2
+- Deve ser vermelho quando: HP-ATUAL < HP/5 (Ativar som de alarme)
+
+Formula de calcular o HP: 
+- (((2 x HP-BASE + IV + (EV/4)) x Nivel) / 100) + Nivel + 10
+
+Formula de calcular os demais status
+- ((((2 x HP-BASE + IV + (EV/4)) x Nivel) / 100) + 5) x Nature
+*/
+
+
+
 class DadosPokedex {
 
-  constructor(numeroPokedex, especie, tipo, sprites, descrição, statusBase) {
-    this.numeroPokedex = numeroPokedex
-    this.especie = especie
-    this.tipo = tipo
-    this.sprites = sprites
-    this.descrição = descrição
-    this.statusBase = statusBase
-
-    /*console.log(
-      `Nº Pokedex: ${numeroPokedex}
-      Espécie: ${especie}
-      Tipo: [${tipo[0]}][${tipo[1]}]
-      Descrição: ${descrição}`
-    )*/
-  }
+	constructor(numeroPokedex, especie, tipo, sprites, statsBase, descricao) {
+		this.numeroPokedex = numeroPokedex
+		this.especie = especie
+		this.tipo = tipo
+		this.sprites = sprites
+		this.descricao = descricao
+		this.statsBase = statsBase
+	}
 }
 
-let N001 = new DadosPokedex('001', 'Bulbassauro', ['Planta', 'Venenoso'], ['pictures/001.png', 'pictures/001S.png'],
+let N001 = new DadosPokedex('001', 'Bulbassauro', ['Planta', 'Venenoso'], ['pictures/001.png', 'pictures/001S.png'], [45, 49, 49, 65, 65, 45],
 `
 Há uma semente de planta em sua parte 
 traseira desde o dia de seu nascimento. 
@@ -31,7 +39,7 @@ A semente cresce lentamente.
 `
 )
 
-let N004 = new DadosPokedex('004', 'Charmander', ['Fogo'], ['pictures/004.png', 'pictures/004S.png'],
+let N004 = new DadosPokedex('004', 'Charmander', ['Fogo'], ['pictures/004.png', 'pictures/004S.png'], [39, 52, 43, 60, 50, 65],
 `
 Charmander, Pokémon Lagarto, desde de 
 seu nascimento Charmander possui uma 
@@ -41,7 +49,7 @@ se ela se apagar, Charmander morrerá.
 `
 )
 
-let N007 = new DadosPokedex('007', 'Squirtle', ['Água'], ['pictures/007.png', 'pictures/007S.png'],
+let N007 = new DadosPokedex('007', 'Squirtle', ['Água'], ['pictures/007.png', 'pictures/007S.png'], [44, 48, 65, 50, 64, 43],
 `
 Há uma semente de planta em sua parte 
 traseira desde o dia de seu nascimento. 
@@ -53,76 +61,107 @@ const pokedex = [N001, N004, N007]
 
 class GerarPokemon {
 
-  constructor(idPokedex, nome, nivel, experiencia) {
-      this.idPokedex = idPokedex
-      this.nome = nome
-      this.nivel = nivel
-      this.experiencia = experiencia
-      //this.nature = this.gerarNature()
+	constructor(idPokedex, nome, nivel, experiencia) {
+		this.idPokedex = idPokedex
+		this.nome = nome
+		this.nivel = nivel
+		this.experiencia = experiencia
 
-      let dadosPokemon = function verificar() {
-        for (let i = 0; i <= pokedex.length; i++) {
-          if (idPokedex == pokedex.numeroPokedex[i]) {
-            return pokedex[i]
-          }
-        }
-      }
+		let dadosPokemon = this.verificar(idPokedex)
 
-      console.log(dadosPokemon)
+		const ivs = this.gerarIVS()
+		const nature = this.gerarNature()
+		const sexo = this.gerarSexoPokemon()
 
-      console.log(
-          `
-          Nome: ${nome}
-          Nivel: ${nivel}
-          Experiência: ${experiencia}
-          IVS: ${this.gerarIVS()}
-          `
+		let stats = this.gerarStats(dadosPokemon.statsBase, ivs, nature, nivel)
 
-          /*
-          Nature: ${nature}
-          
-          
-          Nº Pokedex: ${dadosPokemon.numeroPokedex}
-          Espécie: ${dadosPokemon.especie}
-          Tipo: [${dadosPokemon.tipo[0]}][${dadosPokemon.tipo[1]}]
-          Descrição: ${dadosPokemon.descrição}*/
-      )
-  }
+		console.log(
+			`
+			Nº Pokedex: ${dadosPokemon.numeroPokedex}
+			EXP: ${experiencia}
+			Lv${nivel} ${nome}(${dadosPokemon.especie}) ${sexo}
+			Type: ${dadosPokemon.tipo[0]} | ${dadosPokemon.tipo[1]}
+			Nature: ${nature[0]}
 
-  gerarIVS() {
+			STATS:                IVS:
+			HP:    ${stats[0]}    		  ${ivs[0]}
+			ATK    ${stats[1]}    		  ${ivs[1]}
+			DEF:   ${stats[2]}    		  ${ivs[2]}
+			SATK:  ${stats[3]}    		  ${ivs[3]}
+			SDEF:  ${stats[4]}    		  ${ivs[4]}
+			SPD:   ${stats[5]}    		  ${ivs[5]}
 
-      const ivs = [Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), Math.floor(Math.random() * 32)]
+			Descrição: 
+			${dadosPokemon.descricao}
+			`
+		)
+	}
 
-      /*for (let posicoesIVS of ivs) {
-          posicoesIVS = Math.floor(Math.random() * 32)
-      }*/
+	verificar(idPokedex) {
+		for (let i = 0; i <= pokedex.length; i++) {
+			if (idPokedex == pokedex[i].numeroPokedex) {
+				return pokedex[i]
+			}
+		}
+	}
 
-      console.log(ivs)
+	gerarSexoPokemon() {
+		const randomizador = Math.random() * 10
+		return randomizador > 5 ? "Macho" : "Fêmea"
+	}
 
-      return ivs
-  }
+  	gerarIVS() {
+      	const ivs = [Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), 
+                  	Math.floor(Math.random() * 32), Math.floor(Math.random() * 32), Math.floor(Math.random() * 32)]
 
-  gerarNature() {
-      const vetorNatures = 
-      [
-        ["Hardy", status[0], status[0]], ["Docile", status[0], status[0]], ["Bashful", status[0], status[0]], ["Quirky", status[0], status[0]], ["Serious", status[0], status[0]],
-        ["Adamant", status[1] * 1.10, status[3] * 0.90], ["Lonely", status[1] * 1.10, status[2] * 0.90], ["Naughty", status[1] * 1.10, status[4] * 0.90], ["Brave", status[1] * 1.10, status[5] * 0.90],
-        ["Modest", status[3] * 1.10, status[1] * 0.90], ["Mild", status[3] * 1.10, status[2] * 0.90], ["Rash", status[3] * 1.10, status[4] * 0.90], ["Quiet", status[3] * 1.10, status[5] * 0.90],
-        ["Bold", status[2] * 1.10, status[1] * 0.90], ["Impish", status[2] * 1.10, status[3] * 0.90], ["Lax", status[2] * 1.10, status[4] * 0.90], ["Relaxed", status[2] * 1.10, status[5] * 0.90],
-        ["Calm", status[4] * 1.10, status[1] * 0.90], ["Careful", status[4] * 1.10, status[3] * 0.90], ["Gentle", status[4] * 1.10, status[2] * 0.90], ["Sassy", status[4] * 1.10, status[5] * 0.90],
-        ["Timid", status[5] * 1.10, status[1] * 0.90], ["Jolly", status[5] * 1.10, status[3] * 0.90], ["Hasty", status[5] * 1.10, status[2] * 0.90], ["Naive", status[5] * 1.10, status[4] * 0.90]
-      ];
+    	return ivs
+  	}
 
-      return vetorNatures[teste][Math.random() * 25]
-  }
+	gerarNature() {
+		const vetorNatures = 
+		[
+			["Hardy", 0, 0], ["Lonely", 1, 2], ["Adamant", 1, 3], ["Naughty", 1, 4], ["Brave", 1, 5],
+			["Bold", 2, 1], ["Docile", 0, 0], ["Impish", 2, 3], ["Lax", 2, 4], ["Relaxed", 2, 5],
+			["Modest", 3, 1], ["Mild", 3, 2], ["Bashful", 0, 0], ["Rash", 3, 4], ["Quiet", 3, 5],
+			["Calm", 4, 1], ["Gentle", 4, 2], ["Careful", 4, 3], ["Quirky", 0, 0], ["Sassy", 4, 5],
+			["Timid", 5, 1], ["Hasty", 5, 2], ["Jolly", 5, 3], ["Naive", 5, 4], ["Serious", 0, 0]
+		];
 
-  /*batalhar() {}
+		const nature = vetorNatures[Math.floor(Math.random() * 25)]
 
-  evoluir() {}
+		return nature
+	}
 
-  curar() {}
+	gerarStats(statsBase, ivs, nature, nivel) {
+		var stats = new Array(6)
 
-  subirNivel() {}*/
+		for (let i = 0; i < stats.length; i++) {
+			if (i == 0) {
+				stats[i] = Math.floor(Math.abs(((2 * statsBase[0] + ivs[0] + Math.floor(Math.abs(0 / 4))) * nivel) / 100)) + nivel + 10
+			}
+			else if (nature[1] == i){
+				stats[i] = Math.floor(Math.abs((Math.floor(Math.abs(((2 * statsBase[i] + ivs[i] + Math.floor(Math.abs(0 / 4))) * nivel) / 100)) + 5) * 1.10))
+			}
+			else if (nature[2] == i){
+				stats[i] = Math.floor(Math.abs((Math.floor(Math.abs(((2 * statsBase[i] + ivs[i] + Math.floor(Math.abs(0 / 4))) * nivel) / 100)) + 5) * 0.90))
+			}
+			else {
+				stats[i] = Math.floor(Math.abs((Math.floor(Math.abs(((2 * statsBase[i] + ivs[i] + Math.floor(Math.abs(0 / 4))) * nivel) / 100)) + 5) * 1))
+			}
+		}
+
+		return stats
+	}
+
+	/*batalhar() {}
+
+	evoluir() {}
+
+	curar() {}
+
+	subirNivel() {}*/
 }
 
-let teste = new GerarPokemon('001', 'bulbamon', 5, 0, "", "")
+let teste1 = new GerarPokemon('001', 'bulbamon', 5, 0)
+let teste4 = new GerarPokemon('004', 'charmanda', 5, 0)
+let teste7 = new GerarPokemon('007', 'squirtu', 5, 0)
