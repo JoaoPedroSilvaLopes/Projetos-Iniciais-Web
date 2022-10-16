e0e0e0ffacc3bc4e9b3f771b8d34c657053d6fb7
JoaoPedroSilvaLopes <joaopedroS.L@hotmail.com>
2022-10-16 19:36:31 -0300

    refactor: rename and indentation folder

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,23 +1,21 @@
 function gerar_tabuada() {
+  var valor = document.getElementById("numero");
+  var numero = Number(valor.value);
+  var seletor = document.getElementById("lista");
 
-    var valor = document.getElementById('numero');
-    var numero = Number(valor.value)
-    var seletor = document.getElementById('lista');
+  if (valor.value.length == 0) {
+    window.alert("Valor inválido, insira um número na caixa de entrada");
+  } else {
+    seletor.innerHTML = "";
 
-    if (valor.value.length == 0) {
-        window.alert("Valor inválido, insira um número na caixa de entrada")
+    for (var contador = 1; contador <= 10; contador++) {
+      var multiplicacao = contador * numero;
+      var tabuada = document.createElement("option");
+      tabuada.innerHTML = `${contador} x ${numero} = ${multiplicacao}`;
+      seletor.appendChild(tabuada);
     }
-    else {
-        seletor.innerHTML = '';
 
-        for (var contador = 1; contador <= 10; contador++) {
-            var multiplicacao = contador * numero
-            var tabuada = document.createElement('option');
-            tabuada.innerHTML = `${contador} x ${numero} = ${multiplicacao}`;
-            seletor.appendChild(tabuada);
-        }
-
-        resultado.innerHTML = 'RESULTADO';
-        resultado.appendChild(seletor);
-    }
+    resultado.innerHTML = "RESULTADO";
+    resultado.appendChild(seletor);
+  }
 }
